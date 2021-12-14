efe1e27f72e64dcf344593fe0ed54df8807c6189
CrystalTwoK <marucci.esports@gmail.com>
2021-12-14 20:38:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -351,7 +351,6 @@ function formSubmission() {
 
 function randomKeySound() {
   let randomSound = Math.floor(Math.random() * 6) + 1;
-  console.log(randomSound);
   switch (randomSound) {
     case 1:
       audio.a.play();
